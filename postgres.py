ecba47afb79db29285e145bc9e09f332b7a1cafa
Shirel <shireldavidpor@gmail.com>
2026-03-04 13:27:44 +0200

    adding to Dockerfile_app

--- postgres.py
+++ postgres.py
@@ -6,8 +6,10 @@ import logging, time
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 LOGGER = logging.getLogger(__name__)
 
+
 def get_db_engine():
     return create_engine('postgresql://{}:{}@{}/{}'.format('postgres', 'postgres', 'postgres:5432', 'amongus_db'))
+
 
 while True:
     try:
@@ -15,4 +17,4 @@ while True:
         if db_engine:
             break
     except Exception as e:
-        LOGGER.warning(f"++++ Retrying connection to the db bc of the issue {str(e)}++++"
+        LOGGER.warning(f"++++ Retrying connection to the db bc of the issue {str(e)}++++")
